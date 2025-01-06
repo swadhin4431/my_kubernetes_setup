eece4df91bc1d2d4ef04c27da14803ee97ce036a
swadhin <swadhinpattnaik2105@gmail.com>
2025-01-06 13:20:11 +0530

    modified kube cinfig installation file

--- kubernetes_installation_bash_automation/master_kube.sh
+++ kubernetes_installation_bash_automation/master_kube.sh
@@ -12,6 +12,8 @@ kube_installation () {
         echo "Disabling SELinux..."
         setenforce 0
         sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+    else
+        echo "selinux is already altered"
     fi
 
     # Load necessary kernel modules
@@ -23,8 +25,8 @@ kube_installation () {
         cat > /etc/modules-load.d/k8s.conf << EOF
 overlay
 br_netfilter
-EOF 
-    else:
+EOF
+    else
         echo "kernel module is already loaded"
     fi
 
@@ -37,7 +39,7 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
         sysctl --system
-    else:
+    else
         echo "sysctl is loaded"
     fi
 
@@ -46,39 +48,70 @@ EOF
     if [[ $(swapon --show) ]]; then
         swapoff -a
         sed -e '/swap/s/^/#/g' -i /etc/fstab
+    
     fi
 
     # Install Docker
     echo "Installing Docker..."
     if ! command -v docker &> /dev/null; then
-        dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-        dnf install -y docker-ce
-        systemctl start docker
+        sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+        dnf install -y docker-ce docker-compose-plugin docker-ce-cli
         systemctl enable docker
+        systemctl start docker
+    else
+        echo "docker is already installed"
+        docker ps
     fi
 
     # Install Go
     echo "Installing Go programming language and git utility..."
-    if ! command -v go &> /dev/null && ! command -v git &> /dev/null; then
-        yum install -y go git
-    else:
+    if ! command -v go &> /dev/null || ! command -v git &> /dev/null; then
+        yum install -y go git wget
+    else
         echo "both git and go installed already"
     fi
 
-    # Build and install cri-dockerd
-    echo "Installing cri-dockerd..."
-    if ! command -v cri-dockerd &> /dev/null; then
-        git clone https://github.com/Mirantis/cri-dockerd.git
-        cd cri-dockerd
-        mkdir bin
-        go build -o bin/cri-dockerd
-        mkdir -p /usr/local/bin
-        install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
-        cp -a packaging/systemd/* /etc/systemd/system
-        sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
-        systemctl daemon-reload
-        systemctl enable cri-docker.service
-        systemctl enable --now cri-docker.socket
+    # Build and install crio 
+    echo "Installing crio services..."
+    if ! command -v crio &> /dev/null || [[ ! -f /etc/yum.repos.d/cri-o.repo ]]; then
+        CRIO_VERSION=v1.30
+        cat <<EOF | tee /etc/yum.repos.d/cri-o.repo
+[cri-o]     
+name=CRI-O
+baseurl=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/repodata/repomd.xml.key
+EOF
+#        VERSION=1.22
+#        sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_8/devel:kubic:libcontainers:stable.repo
+#        sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:${VERSION}/CentOS_8/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo
+        sudo dnf -y install cri-o
+        sudo systemctl enable --now crio
+        sudo systemctl start crio
+    else
+        echo "crio is already installed successfully"
+        systemctl list-units | grep crio; crictl ps; cat /etc/crictl.yaml
+        echo "all set from crio CRI prospective"    
+
+    fi
+
+    # add cri socket endpoint to the crictl utilities
+    if ! command -v crictl &> /dev/null || [[ ! -f /etc/crictl.yaml ]]; then
+        echo "crictl is missing or /etc/crictl.yaml does not exist"
+        wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.26.0/crictl-v1.26.0-linux-amd64.tar.gz
+        sudo tar zxvf crictl-v1.26.0-linux-amd64.tar.gz -C /usr/bin
+        cat <<EOF | tee /etc/crictl.yaml
+runtime-endpoint: unix:///var/run/crio/crio.sock
+image-endpoint: unix:///var/run/crio/crio.sock
+timeout: 2
+debug: false
+EOF
+        echo "checking crictl socket connectivity"
+        crictl ps
+    else
+        echo "both crictl & crictl.yaml are already existed"
+        crictl ps
     fi
 
     # Add Kubernetes repository
@@ -87,26 +120,34 @@ EOF
         cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kubelet kubeadm kubectl
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
 EOF
+    else
+       echo "kubernetes.repo is already existed"
     fi
 
     # Install Kubernetes components
     echo "Installing Kubernetes components..."
-    if ! command -v kubeadm &> /dev/null; then
+    if ! command -v kubeadm &> /dev/null || ! command -v kubectl &> /dev/null; then
         dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
         systemctl enable --now kubelet
+        kubeadm version 
+        kubectl version --short
+    else
+        echo "kubeadm,kubectl already installed"
+        kubeadm version
+        kubectl version --short
     fi
 
     # Initialize Kubernetes cluster
     if [[ ! -f /etc/kubernetes/admin.conf ]]; then
         echo "Initializing Kubernetes cluster..."
-        kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr=192.168.0.0/16
+#        kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr=192.168.0.0/16
+        kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/crio/crio.sock --ignore-preflight-errors=SystemVerification,FileExisting-tc,Firewalld
+
 
         # Deploy Calico network plugin
         echo "Deploying Calico network plugin..."
@@ -118,6 +159,18 @@ EOF
         mkdir -p $HOME/.kube
         cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
         chown $(id -u):$(id -g) $HOME/.kube/config
+ #   elif [kubectl get no | grep -i ready]:
+    else
+	 echo "Deploying Calico network plugin..."
+	 curl -L -O https://projectcalico.docs.tigera.io/manifests/calico.yaml
+         kubectl apply -f calico.yaml
+   
+   	 echo "Configuring kubectl for the root user..."
+         mkdir -p $HOME/.kube
+         cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+         chown $(id -u):$(id -g) $HOME/.kube/config
+
+        
     fi
 
     echo "Kubernetes installation completed successfully."
@@ -125,5 +178,4 @@ EOF
 
 # Run the function
 kube_installation
-
 
